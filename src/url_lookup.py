53d2a5b8fa23e7826fd661bb7883998b105e9adf
FlakkenTime <flakkentime@gmail.com>
2019-07-14 19:46:33 -0700

    Updates for url_lookup
    1. add helper function for connecting as its now common
    2. add path for updating DB with new urls

--- src/url_lookup.py
+++ src/url_lookup.py
@@ -1,14 +1,8 @@
-"""
-todo:
-1. logging
-2. shut down
-3. comments
-4. config file
-"""
+#!/usr/bin/python3
 
 import MySQLdb
 from configparser import ConfigParser
-from flask import Flask, Response
+from flask import Flask, Response, request
 
 # setup config for db
 config = ConfigParser()
@@ -17,18 +11,26 @@ config.read('resources/db.ini')
 
 app = Flask(__name__)
 
+def db_connect():
+    """
+    Helper function that connects and returns a db connection
+    """
+    db = MySQLdb.connect(config['Database']['Connect'],
+                          config['Database']['User'],
+                          config['Database']['Password'],
+                          config['Database']['Database_Name'])
+    return db
+
+
 def check_helper(test):
     """
     Helper function that connects to the database and checks if this is safe
     :param test: String. The url to test
     :return: String. True / False
     """
-    db = MySQLdb.connect(config['Database']['Connect'],
-                         config['Database']['User'],
-                         config['Database']['Password'],
-                         config['Database']['Database_Name'])
+    db = db_connect()
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM testing WHERE url = %s LIMIT 1", [test])
+    cursor.execute("SELECT * FROM url_table WHERE url = %s LIMIT 1", [test])
     result = cursor.fetchone()
     db.close()
 
@@ -39,7 +41,7 @@ def check_helper(test):
         # block
         return 'False'
 
-@app.route("/urlinfo/1/<path:test>")
+@app.route("/urlinfo/1/<path:test>", methods=['GET'])
 def url_lookup(test):
     """
     This pulls everything in the url after /urlinfo/1/ and
@@ -52,5 +54,56 @@ def url_lookup(test):
     result = check_helper(test)
     return Response(result), 200
 
+def update_helper(urls):
+    """
+    Pushes url updates to DB
+    :param urls: String. Comma separated urls.
+    :return: (Boolean, String) (True / False, None / Error)
+    """
+    db = db_connect()
+    try:
+        db = db_connect()
+        cursor = db.cursor()
+
+        # Generate our query escaping the URLs. From my reading much faster than
+        # using execute many and having it prepare the query.
+        query = 'INSERT INTO url_table (url) VALUES '
+        url_split = urls.split(',')
+        for url in url_split:
+            query += "('%s')" % db.escape_string(url).decode("utf-8")
+
+        # drop trailing ,
+        query = query[:-1]
+
+        result = cursor.execute(query)
+        db.commit()
+        db.close()
+
+        # Extra check that the number of updates matches the number we received
+        if result == len(url_split):
+            return (True, None)
+        else:
+            return (False, "Incorrect number of updates to db")
+
+    except Exception as e:
+        # Catch the generalized exception
+        db.close()
+        return (False, "Error hit. Check logs")
+
+
+@app.route("/urlupdate/1/", methods=['POST'])
+def url_update():
+    # Pull the json and verify password
+    json = request.get_json()
+    if json['PASS'] != config['Database']['Update_Pass']:
+        return "False", 200
+
+    result = update_helper(json['urls'])
+    if result[0] is True:
+        return "True", 200
+    else:
+        return result[1], 200
+
+
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
