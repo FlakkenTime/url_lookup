7876fda8f0a9936c2ceca5a22c4964b11660defc
FlakkenTime <flakkentime@gmail.com>
2019-07-14 17:56:03 -0700

    Add db.ini file and gunicorn config file

---
+++ resources/config.py
@@ -0,0 +1,16 @@
+import multiprocessing
+
+# general settings
+bind = '127.0.0.1:8080'
+backlog = 2048
+workers = (multiprocessing.cpu_count() * 2) + 1
+worker_class = 'sync'
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+
+# log levels
+errorlog = '-'
+loglevel = 'info'
+accesslog = '-'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
