c68c83d8fb5d3987afc084471d15d444530f25e2
FlakkenTime <flakkentime@gmail.com>
2019-07-14 17:55:12 -0700

    Few updates
    1. prepare the query
    2. add comments
    3. add config file for db

--- src/url_lookup.py
+++ src/url_lookup.py
@@ -7,15 +7,28 @@ todo:
 """
 
 import MySQLdb
+from configparser import ConfigParser
 from flask import Flask, Response
+
+# setup config for db
+config = ConfigParser()
+config.read('resources/db.ini')
 
 
 app = Flask(__name__)
 
 def check_helper(test):
-    db = MySQLdb.connect('localhost', 'fakeName', 'fakePass', 'fakeDB')
+    """
+    Helper function that connects to the database and checks if this is safe
+    :param test: String. The url to test
+    :return: String. True / False
+    """
+    db = MySQLdb.connect(config['Database']['Connect'],
+                         config['Database']['User'],
+                         config['Database']['Password'],
+                         config['Database']['Database_Name'])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM testing WHERE url = '%s' LIMIT 1" % test)
+    cursor.execute("SELECT * FROM testing WHERE url = %s LIMIT 1", [test])
 
     if cursor.fetchone() is None:
         # Allow this request
@@ -26,6 +39,14 @@ def check_helper(test):
 
 @app.route("/urlinfo/1/<path:test>")
 def url_lookup(test):
+    """
+    This pulls everything in the url after /urlinfo/1/ and
+    puts it in the variable test. This is the part we're checking
+    on.
+    Note it drops parameters after ? which shouldn't be needed.
+    :param test: String. The url we're checking for safety
+    :returns: String. True / False
+    """
     result = check_helper(test)
     return Response(result), 200
 
